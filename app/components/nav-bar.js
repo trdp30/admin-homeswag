bc84f19b9a1bae660014bb517e18e12177f30be8
Manoj Upadhyay <manoj.upadhyay@talview.com>
2020-06-10 12:49:05 +0530

    Nav Bar Modified

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,6 +2,11 @@ import Component from '@ember/component';
 import $ from 'jquery';
 
 export default Component.extend({
+
+  didRender (){
+    $('.ui.accordion').accordion();
+  },
+
   actions: {
     toggleMenu() {
       $('.ui.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
