a547eb5f7afabed6a2d582d03cba30eff3484038
Manoj Upadhyay <manoj.upadhyay@talview.com>
2020-06-10 16:30:16 +0530

    Login funtionality

--- app/components/login-view.js
+++ app/components/login-view.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { observer } from '@ember/object';
 import ENV from './../config/environment';
 import fetch from 'fetch';
 import RSVP from 'rsvp';
@@ -12,7 +13,28 @@ export default Component.extend({
   router: service(),
   email: '',
   otp: '',
+  isEmailEmpty: true,
+  isOtpEmpty: true,
+  isOtpResend: false,
+  showAuthenticateButton: false,
   toast: service(),
+
+  isEmptyEmail: observer('email', function() {
+    if(this.get('email') && this.get('email.length')){
+      this.set('isEmailEmpty', false);
+    } else {
+      this.set('isEmailEmpty', true);
+    }
+  }),
+
+  isEmptyOtp: observer('otp', function() {
+    if(this.get('otp') && this.get('otp.length')){
+      this.set('isOtpEmpty', false);
+    } else {
+      this.set('isOtpEmpty', true);
+    }
+  }),
+
 
   isEmailValid(email) {
     var regex = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
@@ -29,6 +51,7 @@ export default Component.extend({
     },
 
     authenticate() {
+      this.set('isOtpDisabled', true)
       this.send('resetField', 'errorEmail')
       let { email, otp } = this.getProperties('email', 'otp');
       if(!email && !email.length) {
@@ -43,22 +66,32 @@ export default Component.extend({
         .then(() => {
           if (this.get('session.isAuthenticated')) {
             this.get('router').replaceWith('dashboard')
+            this.send('clearTimer');
             // What to do with all this success?
           }
         }).catch((error) => {
           this.set('isDisabled', false)
+          this.set('isOtpDisabled', false)
           this.set('errorMessage', error.message || error);
           this.get('toast').error(error.message || error)
         })
       }
     },
+
     getOtp() {
+      this.set('isOtpDisabled', true)
+      this.set('otp', null)
       this.set('errorEmail', null)
       if(!this.get('email') && !this.get('email').length) {
         return this.send('setErrorField', 'errorEmail', "This field is required");
       }
       if(this.isEmailValid(this.get('email'))) {
         this.set('isDisabled', true)
+        this.set('isEmailDisabled', true)
+        let timeInterval = setTimeout(() => {
+          this.set('isOtpResend', true);
+        }, 10000)
+        this.set('timeInterval', timeInterval)
         const options = {
           body : JSON.stringify({ email: this.get('email')}),
           method: 'POST'
@@ -71,10 +104,13 @@ export default Component.extend({
                 if (!response.ok) {
                   response.responseJSON = json;
                   this.set('isDisabled', false)
+                  this.set('isEmailDisabled', false)
+                  this.set('isOtpDisabled', false)
                   reject(json);
                 } else {
                   this.set('isDisabled', false)
-                  this.send('toggleView', 'showAuthenticateButton')
+                  this.set('showAuthenticateButton', true)
+                  this.set('isOtpDisabled', false)
                   resolve(json);
                 }
               } catch (SyntaxError) {
@@ -85,18 +121,23 @@ export default Component.extend({
           }).catch(reject);
         }).catch((e) => this.send('catchError', e))
       } else {
+        this.set('isEmailDisabled', false)
         return this.send('setErrorField', 'errorEmail', "Invalid email address");
       }
     },
 
     catchError(error) {
       this.set('isDisabled', false)
-      console.log(error)
-      this.get('toast').error('error.message')
+      this.get('toast').error(error.message)
+    },
+
+    clearTimer() {
+      clearTimeout(this.get('timeInterval'));
     },
 
     toggleView(property) {
       this.toggleProperty(property)
+      this.set('isEmailDisabled', false)
     }
   }
 });
